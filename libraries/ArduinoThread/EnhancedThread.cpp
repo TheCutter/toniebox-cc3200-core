3ff6d2c6aee8024bd8b75445bd6481ce0710335d
scilor <SciLor@users.noreply.github.com>
2020-06-02 07:54:16 +0200

    Added ArduinoThread

---
+++ libraries/ArduinoThread/EnhancedThread.cpp
@@ -0,0 +1,24 @@
+#include "EnhancedThread.h"
+
+void EnhancedThread::run() {
+  Thread::run();
+  if (_runOnce)
+    Thread::enabled = false;
+}
+
+void EnhancedThread::runIfNeeded(void) {
+  if(Thread::shouldRun())
+    Thread::run();
+}
+
+void EnhancedThread::reset(void) {
+  Thread::enabled = true;
+  Thread::runned();
+}
+
+void EnhancedThread::setRunOnce(bool runOnce) {
+  _runOnce = runOnce;
+}
+unsigned long EnhancedThread::getInterval(void) {
+  return interval;
+}
---
+++ libraries/ArduinoThread/EnhancedThread.h
@@ -0,0 +1,19 @@
+#ifndef EnhancedThread_h
+#define EnhancedThread_h
+
+#include "BaseHeader.h"
+#include <Thread.h>
+
+class EnhancedThread : public Thread {
+  public:
+    void
+      run(void),
+      runIfNeeded(void),
+      reset(void),
+      setRunOnce(bool);
+    unsigned long getInterval(void);
+  private:
+    bool _runOnce;
+};
+
+#endif
